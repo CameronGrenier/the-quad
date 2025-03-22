fe2ae14eec10123b6d6282823dc65ae759297814
CameronGrenier <camerongrenier12345@gmail.com>
2025-03-21 22:24:03 -0400

    rename logo field to thumbnail in organization registration form

--- client/src/components/OrganizationRegistration.js
+++ client/src/components/OrganizationRegistration.js
@@ -258,7 +258,7 @@ function OrganizationRegistration() {
             <div className="custom-file-upload">
               <input
                 type="file"
-                name="Thumbnail"
+                name="thumbnail"
                 onChange={handleChange}
                 accept="image/*"
               />
@@ -268,16 +268,16 @@ function OrganizationRegistration() {
                 </svg>
                 Choose Thumbnail
               </div>
-              {formData.logo && (
+              {formData.thumbnail && (
                 <div className="file-name">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                     <path d="M21 19V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2zM8.5 13.5l2.5 3.01L14.5 12l4.5 6H5l3.5-4.5z"/>
                   </svg>
-                  {formData.logo.name}
+                  {formData.thumbnail.name}
                 </div>
               )}
             </div>
-            {errors.logo && <p className="error">{errors.logo}</p>}
+            {errors.thumbnail && <p className="error">{errors.thumbnail}</p>}
           </div>
           <div className="form-group">
             <label>Banner:</label>
