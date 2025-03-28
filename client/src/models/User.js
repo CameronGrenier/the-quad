f5deb06b17a8c8d6a93abb2a64eeccb3fcbf6d63
Humayoun <humayounkhan09@outlook.com>
2025-03-27 22:45:39 -0400

    Pseudocode

--- client/src/models/User.js
+++ client/src/models/User.js
@@ -14,7 +14,21 @@ export class User {
  //Create Account function implemented in account controller class
  //Moving modify account settings to account controller class
   async modifyAccountSettings() {
-   /* implement as needed */ 
-   //this is a test of kcode commitment
+    /*
+    // What does this function do?
+    * Takes a form object and modifies the account settings of the user
+    *Framework: 
+    * Take the form object and parse it
+    * Error Checks: 
+    * If picture feild is empty, set it to the default picture
+    * Throw errors:
+    * (Check DB for duplicates)
+    Username already exists
+    Email already exists
+
+    Errors resolved:
+    use database service to save the new info into the user table
+    */
+  
   }
-  }
+}
